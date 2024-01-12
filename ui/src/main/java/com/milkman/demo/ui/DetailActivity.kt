165c5e8140b64a618d0261d79e0ee45af96ab594
Augusto Picciani <augustopicciani@gmail.com>
2024-01-12 17:24:52 +0100

    add loader to details activity

--- ui/src/main/java/com/milkman/demo/ui/DetailActivity.kt
+++ ui/src/main/java/com/milkman/demo/ui/DetailActivity.kt
@@ -3,10 +3,12 @@ package com.milkman.demo.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
 import androidx.activity.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import com.airbnb.lottie.LottieAnimationView
 import com.bumptech.glide.Glide
 import com.google.android.material.snackbar.Snackbar
 import com.milkman.demo.model.BeerModel
@@ -22,6 +24,8 @@ class DetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailBinding
     private val viewModel: DetailViewModel by viewModels()
     private var beerId : String? = null
+    private lateinit var  loader : LottieAnimationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
@@ -30,7 +34,10 @@ class DetailActivity : AppCompatActivity() {
     }
 
     private fun setup(){
+
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        loader = binding.loaderIncludeView.loader
 
         val bundle = intent.getBundleExtra(Constants.BUNDLE_NAME)
         bundle?.let {
@@ -42,13 +49,14 @@ class DetailActivity : AppCompatActivity() {
                 viewModel.beerResultStateFlow.collect{ result->
                     when (result) {
                         is ResultState.Success -> {
+                            hideLoader()
                            populateUI(result.data)
                         }
                         is ResultState.Failure -> {
                             Snackbar.make(binding.root, result.exception.message.toString(), Snackbar.LENGTH_LONG).show()
                         }
                         is ResultState.Empty -> {}
-                        is ResultState.Loading -> {}
+                        is ResultState.Loading -> {showLoader()}
                     }
 
 
@@ -69,6 +77,16 @@ class DetailActivity : AppCompatActivity() {
             .with(this)
             .load(beerModel.image_url)
             .into(binding.beerImage)
+    }
+
+    private fun showLoader(){
+        loader.visibility = View.VISIBLE
+        loader.playAnimation()
+    }
+
+    private fun hideLoader(){
+        loader.visibility = View.GONE
+        loader.pauseAnimation()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
